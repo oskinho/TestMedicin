f1c6fcc848974a808815476305054137987904c3
Oskar <eaaoska@students.eaaa.dk>
2022-09-23 09:58:14 +0200

    Dagligskaev test oprettet

--- test/ordination/DagligSkaevTest.java
+++ test/ordination/DagligSkaevTest.java
@@ -1,8 +1,8 @@
 package ordination;
 
+import org.junit.Test;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import java.time.LocalDate;

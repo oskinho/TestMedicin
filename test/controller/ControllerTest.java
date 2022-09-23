01c6a5bb26faf26bca8bf699689f3cec005f1496
RikkeLP <104197400+RikkeLP@users.noreply.github.com>
2022-09-23 10:06:58 +0200

    TC1_OpretDagligFastOrdination test metode

--- test/controller/ControllerTest.java
+++ test/controller/ControllerTest.java
@@ -1,6 +1,10 @@
 package controller;
 
+import ordination.Laegemiddel;
+import ordination.Patient;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,7 +15,23 @@ class ControllerTest {
     }
 
     @Test
-    void opretDagligFastOrdination() {
+    void TC1_opretDagligFastOrdination_SlutdatoEfterStartdato() {
+        //Arrange
+        Controller c = Controller.getController();
+        LocalDate startDato = LocalDate.of(2022, 9, 22);
+        LocalDate slutDato = LocalDate.of(2022, 9, 29);
+        Patient p1 = new Patient("310894-2314", "Jane Hansen", 70);
+        Laegemiddel lgm1 = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+        double morgenAntal = 3;
+        double middagAntal = 2;
+        double aftenAntal = 1;
+        double natAntal = 1;
+
+        //Act
+        c.opretDagligFastOrdination(startDato, slutDato, p1, lgm1, morgenAntal, middagAntal, aftenAntal, natAntal);
+
+        //Assert
+
     }
 
     @Test
